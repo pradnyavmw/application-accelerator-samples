652375fa19431d95254adedb678107bedf78e760
pradnyavmw <dpradnya@vmware.com>
2023-07-12 14:37:45 -0400

    add hello func

--- tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
@@ -3,8 +3,12 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+///Users/dpradnya/workspace/ALM/mydemos/application-accelerator-samples/tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
+
 @RestController
 public class HelloController {
+	@Value("${TARGET:MyWorld}")
+  	String target;
 
 	@RequestMapping("/")
 	public String index() {
@@ -12,5 +16,11 @@ public class HelloController {
 		//return "Greetings from Spring Boot-  VMWARE!";	
 		return "Greetings from Spring Boot-  Runtime!";	
 	}
-
+	@RequestMapping("/hello")
+	public String hello(){
+      return "Hello000000-" + target + "!";
+	}
 }
+
+
+ 
