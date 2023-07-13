deccc2c30d54a84108624600d821275e36f8f139
pradnyavmw <dpradnya@vmware.com>
2023-07-13 17:37:38 -0400

    add greeting

--- node-express/server.js
+++ node-express/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const greeting = process.env.GREETING || "WORLD";
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  res.send('Hello from server to....' + greeting);
 });
 
 app.get('/health', (req, res) => {
@@ -13,3 +14,21 @@ app.get('/health', (req, res) => {
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 });
+
+
+
+// const greeting = process.env.GREETING || "WORLD";
+// var http = require("http");
+
+// http.createServer(function (request, response) {
+//     // Send the HTTP header 
+//     // HTTP Status: 200 : OK
+//     // Content Type: text/plain
+//     response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+//     // Send the response body as "Hello World"
+//     response.end('Hellooooo...' + greeting);
+// }).listen(8081);
+
+// // Console will print the message
+// console.log('Server running at http://127.0.0.1:8081/');
