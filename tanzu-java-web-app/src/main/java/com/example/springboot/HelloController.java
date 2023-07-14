559bdcc0a6fa7fb697d613933bd8a3fd0a57d25e
pradnyavmw <dpradnya@vmware.com>
2023-07-13 21:57:35 -0400

    add msg

--- tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.springboot;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+ 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,14 +11,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-	// @Value("${TARGET:MyWorld}")
-	// String target;
+	@Value("${TARGET:MyWorld}")
+	String target;
 
+
+Read more: https://javarevisited.blogspot.com/2012/08/how-to-get-environment-variables-in.html#ixzz87Ome5g8F
 	@RequestMapping("/")
 	public String index() {
+  		String message = System.getenv("TARGET");
+		if(message==null)
+		{	
+			message="WORLD";
+		}
+		System.out.println("TARGET : "  + message);
+
+
 		// return "Greetings from Spring Boot- edit1git t + Tanzu!";
 		// return "Greetings from Spring Boot- VMWARE!";
-		return "Greetings from Spring Boot-  Vmware!";
+		//return "Greetings from Spring Boot-  Vmware!";
+		return "Greetings from Spring Boot- " + message;
 		// return "Greetings from " + target;
 	}
 	// @RequestMapping("/hello")
